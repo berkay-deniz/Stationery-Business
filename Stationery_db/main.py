ab8339934d0f1f35f745cdaee153db8421170c22
VahapGozenelioglu <74670783+VahapGozenelioglu@users.noreply.github.com>
2021-01-09 20:30:03 +0300

    Server name edit

--- Stationery_db/main.py
+++ Stationery_db/main.py
@@ -1,10 +1,10 @@
 from flask import Flask,render_template,flash,redirect,url_for,session,logging,request
 import pyodbc
 from wtforms import Form,StringField,TextAreaField,PasswordField,validators,IntegerField,DateField
-
+#VG DESKTOP-CPMCPBA
 conn = pyodbc.connect(
     "Driver={SQL Server};"
-    "Server=LAPTOP-HCAE3FVL\MSSQLSERVER01;"
+    "Server=DESKTOP-CPMCPBA;"
     "Database=STATIONERY_BUSINESS;"
     "Trusted_Connection=yes;"
 )
