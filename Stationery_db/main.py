c5e9ed84df95df3aa39da470fff11405d5cb28ed
xbrkyx <brkydnz89@gmail.com>
2021-01-09 12:48:04 +0300

    setup

--- Stationery_db/main.py
+++ Stationery_db/main.py
@@ -1,10 +1,45 @@
 from flask import Flask,render_template
+import pyodbc
+
+conn = pyodbc.connect(
+    "Driver={SQL Server};"
+    "Server=LAPTOP-HCAE3FVL\MSSQLSERVER01;"
+    "Database=STATIONERY_BUSINESS;"
+    "Trusted_Connection=yes;"
+)
+
+def readProductType(conn):
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM PRODUCT_TYPE')
+    for row in cursor:
+        print(f'{row}')
+    
+
+def insertProductType(conn, product_type):
+    cursor=conn.cursor()
+    cursor.execute(
+        'insert into PRODUCT_TYPE (TypeName) values (?)',
+        (product_type)
+    )
+    conn.commit()
+    print("Inserted")
+
+
+def updateProductType(conn, id, type_name):
+    cursor=conn.cursor()
+    cursor.execute(
+        'Update PRODUCT_TYPE set TypeName = ? Where id = ?',(type_name,id)
+    )
+    conn.commit()
+    print('Updated')
+
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    #readProductType(conn)
     return render_template("index.html")
 
 @app.route("/about")
@@ -21,4 +56,16 @@ def VG():
 
 if __name__ ==  "__main__":
     app.run(debug=True)
+
+
+
+#readProductType(conn)
+#insertProductType(conn, "test 2")
+#updateProductType(conn, 33, 'New Test')
+
+
+
+
+
+conn.close()
 
